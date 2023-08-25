dfbb5dc33ffb3236d4ed72394e3083364c1fad36
Bizzrecruit <bizzrecruit@gmail.com>
2023-08-25 21:54:23 +0530

    update

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const axios = require('axios');
-const security = require('./security'); // Adjust the path to security.js
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Load environment variables
-require('dotenv').config();
-const encryptedAccessToken = process.env.INSTAGRAM_ENCRYPTED_TOKEN; // Encrypted token
+// Set up static files (CSS, images)
+app.use(express.static('public'));
 
-// ... Set up static files and view engine ...
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
+// Route to fetch and display Instagram posts
 app.get('/', async (req, res) => {
     try {
-        const accessToken = security.decryptToken(encryptedAccessToken); // Decrypt the token
+        const accessToken = 'IGQWROaE9nOWczMG1EZAnE0NmJldUZAtdE52a3hXRUFmRjkxODdXcU5sa0pHNzA2dkx3TUdYdHg3S0VMUHVFSzlJODRYaW8wbzlBRUFTVjBQUGVhZAkJqRUQ5ZAHdZAa0NSQWpXb3pndWJPdlMydWNzR3ZARZAFdOMzlmaVUZD'; // Replace with your actual token
         const response = await axios.get(`https://graph.instagram.com/me/media?fields=media_type,media_url,thumbnail_url,caption&access_token=${accessToken}`);
         const posts = response.data.data;
         res.render('index', { posts });
