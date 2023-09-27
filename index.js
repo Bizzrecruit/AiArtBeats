28538d3f7743ddf72b5e9ba4eb424f83258338f0
Bizzrecruit <bizzrecruit@gmail.com>
2023-09-27 16:05:18 +0530

    index update

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-//app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static('public'));
 
 
 // Define a route to retrieve a list of images from the "public/images" folder
 app.get('/images', (req, res) => {
   const fs = require('fs');
-  const imageFolder = 'public/images'; // Replace with your folder path
+  const imageFolder = './public/images'; // Replace with your folder path
 
   fs.readdir(imageFolder, (err, files) => {
     if (err) {
